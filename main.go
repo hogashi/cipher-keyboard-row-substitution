d2e465f1921ddde8686c797cdcd5c8cced5a8dde
hogashi <azuma.ginkakuji@gmail.com>
2022-05-20 23:24:31 +0900

    keyboard row substitution

--- main.go
+++ main.go
@@ -4,12 +4,29 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+// 1234567890
+// qwertyuiop
+// asdfghjkl;
+// zxcvbnm,./
+var keyboardNextChar = map[rune]rune{
+	'1': 'q', '2': 'w', '3': 'e', '4': 'r', '5': 't', '6': 'y', '7': 'u', '8': 'i', '9': 'o', '0': 'p',
+	'q': 'a', 'w': 's', 'e': 'd', 'r': 'f', 't': 'g', 'y': 'h', 'u': 'j', 'i': 'k', 'o': 'l', 'p': ';',
+	'a': 'z', 's': 'x', 'd': 'c', 'f': 'v', 'g': 'b', 'h': 'n', 'j': 'm', 'k': ',', 'l': '.', ';': '/',
+	'z': '1', 'x': '2', 'c': '3', 'v': '4', 'b': '5', 'n': '6', 'm': '7', ',': '8', '.': '9', '/': '0',
+}
+
+func substitution(r rune) rune {
+	return keyboardNextChar[r]
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		text := scanner.Text()
+		fmt.Println(strings.Map(substitution, text))
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
